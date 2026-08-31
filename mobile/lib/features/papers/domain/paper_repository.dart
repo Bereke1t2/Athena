41e85e0fea86ff1c420fdea3c2e5c1a3d751e226
bereke1t2 <bereketaschalew1212@gmail.com>
2026-08-31 18:57:00 +0000

    feat(mobile): add article reader and audio playback domain models & repositories

--- mobile/lib/features/papers/domain/paper_repository.dart
+++ mobile/lib/features/papers/domain/paper_repository.dart
@@ -1,6 +1,8 @@
+import 'article_content.dart';
 import 'paper.dart';
 
 /// Repository interface lives in feature domain/ (conventions rule 4).
 abstract interface class PaperRepository {
   Future<PaperDetail> getById(String id);
+  Future<ArticleContent> getArticleContent(String id);
 }
